211331c3fb4659bb88cb644cf78e0105ceeeb79a
nimeshmaharjan1 <maharjannimesh11@gmail.com>
2022-11-11 23:51:54 +0545

    build: initial configuration and setup

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+const options = {
+  arrowParens: "avoid",
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  tabWidth: 2,
+  tralingComma: "none",
+};
+
+module.exports = options;
